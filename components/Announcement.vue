86e704f078ed5fe21c01abca1ed7c5ec20827197
tommytrg <tomastrg22@gmail.com>
2021-03-23 16:06:22 +0100

    chore(i18n): internationalize content

--- components/Announcement.vue
+++ components/Announcement.vue
@@ -2,11 +2,11 @@
   <SectionLayout class="announcement-section">
     <div class="wrapper">
       <div class="announcement-content">
-        <p class="tag">ANNOUNCEMENT</p>
-        <h4 class="subtitle">Witnet is now side-chanined to Ethereum</h4>
+        <p class="tag">{{ $t('announcement.tag') }}</p>
+        <h4 class="subtitle">{{ $t('announcement.subtitle') }}</h4>
       </div>
-      <Button color="black" :font-size="16">
-        > Learn how to use Witnet from a solidity contract
+      <Button color="plain" :font-size="16">
+        {{ $t('announcement.button') }}
       </Button>
     </div>
   </SectionLayout>
--- components/JoinCommunity.vue
+++ components/JoinCommunity.vue
@@ -1,6 +1,6 @@
 <template>
   <SectionLayout class="join-community-section">
-    <h4>Questions? Join the Telegram and Discord communities!</h4>
+    <h4 class="text">{{ $t('join_community.text') }}</h4>
   </SectionLayout>
 </template>
 
