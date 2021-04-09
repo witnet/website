3888e52cfd9b2e67dccd224303fcbe32314aec3c
tommytrg <tomastrg22@gmail.com>
2021-04-09 16:42:53 +0200

    fix(i18n): use browser locale by default

--- default.js
+++ default.js
@@ -1,1 +1,2 @@
+export const fallbackLocale = 'en'
 export const defaultLocale = 'en'
---
+++ pages/_lang/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <Hero />
+    <DecentralizedOracle id="oracle" />
+    <WitToken id="token" />
+    <Ecosystem id="ecosystem" />
+    <GetInvolved id="get_involved" />
+    <JoinCommunity id="community" />
+    <Footer />
+  </div>
+</template>
