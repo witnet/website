0ff7910544b46bbaeae8e8b96eee6a4fce78ba34
tommytrg <tomastrg22@gmail.com>
2023-12-12 19:44:41 +0100

    chore: add empty boilerplate with nuxt3 to witnet 2.0

---
+++ constants.ts
@@ -0,0 +1,7 @@
+export const urls: Record<string, string> = {
+  telegram: 'https://t.me/witnetio',
+  discord: 'https://discord.gg/witnet',
+  twitter: 'https://twitter.com/witnet_io',
+  github: 'https://github.com/witnet/my-wit-wallet/issues',
+  email: 'mailto:maria@otherplane.com',
+}
