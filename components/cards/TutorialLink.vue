dbba31741b14776399c42c7cc2644de618f33f4a
gabaldon <maria@otherplane.com>
2024-01-02 18:14:56 +0100

    feat: add content to the build route

---
+++ components/cards/TutorialLink.vue
@@ -0,0 +1,17 @@
+<template>
+  <a :href="url">
+    <h4>{{ name }}</h4>
+  </a>
+</template>
+<script setup lang="ts">
+defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+})
+</script>
