c75c8eab6eabff03e07bb20a7f3ba84eb6a49a4c
gabaldon <maria@otherplane.com>
2024-07-15 16:34:12 +0200

    hero-navbar

--- constants.ts
+++ constants.ts
@@ -1,28 +1,36 @@
 import { localeCodes, type LanguageDictionary } from './types'
 
 export const URLS: Record<string, string> = {
-  telegram: 'https://t.me/witnetio',
-  discord: 'https://discord.gg/witnet',
-  twitter: 'https://twitter.com/witnet_io',
-  github: 'https://github.com/witnet/my-wit-wallet/issues',
-  dataFeedsExplorer: 'https://feeds.witnet.io',
-  blockExplorer: 'https://witnet.network/',
+  ado: 'https://github.com/adoracles/',
   advocateProgram:
     'https://witnet.notion.site/The-Witnet-Advocate-Program-f572baff6b1d49a69d4db785a924fc61',
+  bitmart: 'https://www.bitmart.com/trade/en-US?symbol=WIT_USDT',
+  block_explorer: 'https://witnet.network/',
+  changelly: '#',
+  data_feeds_explorer: 'https://feeds.witnet.io',
+  discord: 'https://discord.gg/witnet',
+  docker_node: 'https://hub.docker.com/r/witnet/witnet-rust/tags',
+  documentation: 'https://docs.witnet.io/',
+  gate: 'https://www.gate.io/trade/WIT_USDT',
+  github: 'https://github.com/witnet',
   grantProgram:
     'https://witnet.notion.site/The-Witnet-Grant-Program-0e6bc5ddbe4a4bf8a22c262dedfe268f',
+  letsexchange: 'https://letsexchange.io/coin/wit',
+  linux_node: 'https://github.com/witnet/witnet-rust/releases',
+  macos_node: 'https://github.com/witnet/witnet-rust/releases',
+  mediaKit:
+    'https://drive.google.com/drive/u/1/folders/1z9dSts64xyaIZqRvniMPDib7UWOE2P3z',
+  medium: 'https://medium.com/witnet',
+  mexc: 'https://www.mexc.com/exchange/WIT_USDT',
+  my_wit_wallet: 'https://mywitwallet.com/',
   newsletter: 'http://eepurl.com/iDxYzs',
-  bitmart: '',
-  changelly: '',
-  gateio: '',
-  mexc: '',
-  simpleSwap: '',
-  stakeTutorial: '',
-  dockerNode: '',
-  windowsNode: '',
-  macosNode: '',
-  linuxNode: '',
-  rasberrypiNode: '',
+  rasberrypi_node: 'https://github.com/witnet/witnet-rust/releases',
+  reddit: 'https://reddit.com/r/witnet',
+  simpleSwap: 'https://simpleswap.io/coins/wit',
+  telegram: 'https://t.me/witnetio',
+  windows_node: 'https://github.com/witnet/witnet-rust/releases',
+  witnet_foundation: 'https://witnet.foundation',
+  x: 'https://twitter.com/witnet_io',
 }
 
 export const languageLocales: LanguageDictionary = {
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="p-6 w-full grid grid-rows-[max-content_1fr_max-content] bg-white dark:text-wit-blue-500 min-h-screen flex"
+    class="p-6 w-full grid grid-rows-[max-content_1fr_max-content] bg-white dark:text-wit-blue-500 min-h-screen"
   >
     <NuxtLayout name="navbar">
       <TopBar :nav-links="navLinks">
@@ -11,17 +11,17 @@
         </template>
         <template v-for="link in navLinks" :key="link.key" #[link.key]>
           <InnerLink :hash="link.key">
-            <span class="slash">/</span>{{ link.locale }}
+            <span class="slash text-wit-blue-500">/</span>{{ link.locale }}
           </InnerLink>
         </template>
       </TopBar>
-      <SocialLinks class="fixed sm:hidden" />
+      <!-- <SocialLinks class="fixed sm:hidden" /> -->
     </NuxtLayout>
     <!-- <Compoennt /> -->
     <!-- <client-only>
       <ThemeSwitch class="theme-switch" />
     </client-only> -->
-    <div class="grid grid-auto mt-16">
+    <div class="grid grid-auto mt-8">
       <slot></slot>
     </div>
     <!-- <FooterSection /> -->
@@ -35,28 +35,22 @@ const { t } = useI18n()
 
 const navLinks: Array<{ key: string }> = [
   {
-    key: 'oracle',
-    to: '/oracle',
-    rel: 'oracle',
-    locale: t('nav_bar.route.oracle'),
+    key: 'stake',
+    to: '/stake',
+    rel: 'stake',
+    locale: t('nav_bar.route.stake'),
   },
   {
-    key: 'coin',
-    to: '/coin',
-    rel: 'coin',
-    locale: t('nav_bar.route.coin'),
+    key: 'buy',
+    to: '/buy',
+    rel: 'buy',
+    locale: t('nav_bar.route.buy'),
   },
   {
-    key: 'apps',
-    to: '/apps',
-    rel: 'apps',
-    locale: t('nav_bar.route.apps'),
-  },
-  {
-    key: 'community',
-    to: '/community',
-    rel: 'community',
-    locale: t('nav_bar.route.community'),
+    key: 'build',
+    to: '/build',
+    rel: 'build',
+    locale: t('nav_bar.route.build'),
   },
 ]
 </script>
