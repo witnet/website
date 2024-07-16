8d5eccb89fa1f3d3e3c72279b856d55c140d0a37
gabaldon <maria@otherplane.com>
2024-07-16 12:29:48 +0200

    feat(SecureSection): implement section

---
+++ components/ArrowButton.vue
@@ -0,0 +1,33 @@
+<template>
+  <CustomButton :type="ButtonType.terciary">
+    <span class="flex items-center">
+      <slot></slot>
+      <ArrowRightIcon class="arrow ml-md"></ArrowRightIcon>
+    </span>
+  </CustomButton>
+</template>
+
+<script setup>
+import { ButtonType } from '@/types'
+
+import ArrowRightIcon from '@/assets/svg/arrow_right.svg?component'
+defineProps({
+  type: {
+    type: String,
+    default: ButtonType.primary,
+    validator(value) {
+      return Object.values(ButtonType).includes(value)
+    },
+  },
+})
+</script>
+
+<style scoped>
+.btn {
+  font-size: 1rem;
+}
+
+.arrow {
+  height: 14px;
+}
+</style>
