8f904b361ec7ed5cff2ade9b3fa577f00fad3d97
gabaldon <maria@otherplane.com>
2024-07-17 12:25:28 +0200

    explore section

--- components/sections/ExploreSection.vue
+++ components/sections/ExploreSection.vue
@@ -1,24 +1,37 @@
 <template>
-  <div class="!border-black section w-full flex justify-center p-xl">
-    <div class="content grid grid-cols-3">
-      <div class="flex">
-        <ArrowTurnRightIcon class="arrow m-md"/>
-        <!-- <h2 class="title-h1 mr-sm">{{ $t('explore.title') }}</h2> -->
+  <NuxtLayout
+    name="section"
+    :frame-classes="'bg-wit-blue-500 px-[0px]'"
+    :content-classes="'grid grid-cols-2 sm:grid-cols-1 md:grid-cols-1 justify-items-center'"
+  >
+    <template #content>
+      <div class="flex self-center sm:w-96 justify-self-center">
+        <ArrowTurnRightIcon class="arrow m-md" />
 
-        <i18n-t keypath="explore.title.main" class="title-h2 mr-sm white" tag="h2">
+        <i18n-t
+          keypath="explore.title.main"
+          class="title-h2 font-bold mr-0 sm:mr-sm text-white-50"
+          tag="h2"
+        >
           <span class="text-black-950">{{ $t('explore.title.witnet') }}</span>
         </i18n-t>
       </div>
-      
-      <ExplorerLink
-        v-for="explorer in explorers"
-        :key="explorer.title"
-        :title="explorer.title"
-        :description="explorer.description"
-        :url="explorer.url"
-      />
-    </div>
-  </div>
+
+      <div class="grid gap-md mt-lg">
+        <ExplorerLink
+          v-for="explorer in explorers"
+          :key="explorer.title"
+          class="m-sm min-w-80"
+          shadow-color="black"
+          :title="explorer.title"
+          :url="explorer.url"
+          ><template #description
+            ><p class="text-black-950">{{ explorer.description }}</p></template
+          ></ExplorerLink
+        >
+      </div>
+    </template>
+  </NuxtLayout>
 </template>
 
 <script setup lang="ts">
@@ -35,34 +48,18 @@ const explorers: Array<Explorer> = [
   {
     title: t('explore.explorer-1.title'),
     description: t('explore.explorer-1.description'),
-    url: URLS.dataFeedsExplorer,
+    url: URLS.data_feeds_explorer,
   },
   {
     title: t('explore.explorer-2.title'),
     description: t('explore.explorer-2.description'),
-    url: URLS.blockExplorer,
+    url: URLS.block_explorer,
   },
 ]
 </script>
 
 <style scoped>
-.white {
-  color: white;
-}
-
 .arrow {
-  width: 90px
-}
-
-.title-h1 {
-  color: white;
-}
-
-.section {
-  background: #00E2ED;
-}
-
-.content {
-  max-width: 1100px;
+  width: 90px;
 }
 </style>
