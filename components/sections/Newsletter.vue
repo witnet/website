fccf49c1da526e2d2c133d474101dfcc18cf24f0
gabaldon <maria@otherplane.com>
2024-07-17 16:37:38 +0200

    newsletter section

--- components/sections/Newsletter.vue
+++ components/sections/Newsletter.vue
@@ -1,40 +1,39 @@
 <template>
-  <div
-    class="blue grid grid-cols-[repeat(2,1fr)] items-center justify-items-end"
+  <NuxtLayout
+    name="section"
+    :frame-classes="'bg-wit-blue-500'"
+    :content-classes="'grid justify-start gap-sm'"
   >
-    <div
-      class="col-start-1 col-span-1 flex flex-col w-full items-center grid-cols"
-    >
-      <h3 class="title-h3 mb-sm">{{ t('newsletter.title') }}</h3>
-      <div class="flex mb-xl">
-        <p class="text">{{ $t('newsletter.subtitle') }}</p>
-        <DashesIcon class="col-start-2 col-span-2 dashes" />
+    <template #content>
+      <h2 class="title-h2">{{ t('newsletter.title') }}</h2>
+      <div
+        class="grid grid-cols-[max-content_1fr] gap-md justify-items-start items-center"
+      >
+        <p class="text text-xl">{{ $t('newsletter.subtitle') }}</p>
+        <DashesIcon class="w-[80px] h-auto" />
       </div>
-      <!-- 
-      <div>
+      <div class="mt-lg">
         <label for="subscribe_buttom" class="sr-only">Label</label>
         <div class="flex rounded-lg shadow-sm">
           <input
-            type="text"
             id="subscribe_buttom"
+            type="text"
             name="subscribe_buttom"
             placeholder="name@domain.com"
-            class="py-sm px-md block w-full border-gray-200 shadow-sm rounded-s-lg text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+            class="rounded-l-full min-w-[300px] sm:min-w-max py-sm px-md block w-full border-gray-200 shadow-sm rounded-s-lg text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
           />
           <button
             type="button"
-            class="py-sm px-md inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-e-md border border-transparent bg-black-950 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
+            class="rounded-r-full py-sm px-md inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-e-md border border-black-950 bg-black-950 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
           >
-           Subscribe 
+            Subscribe
           </button>
         </div>
-      </div> -->
-      <Mailchimp />
- 
-    </div>
-
-    <PalmBlackIcon class="col-start-2 col-span-2 palm hidden" />
-  </div>
+      </div>
+        <!-- <Mailchimp /> -->
+      <PalmBlackIcon class="col-start-2 col-span-2 palm hidden" />
+    </template>
+  </NuxtLayout>
 </template>
 
 <script setup lang="ts">
