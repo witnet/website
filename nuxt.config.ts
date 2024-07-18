a1b0355a8c74a6cf2aa19ab73a3890d99b9a0548
gabaldon <maria@otherplane.com>
2024-07-18 18:03:47 +0200

    feat: add metadata

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,101 @@ import svgLoader from 'vite-svg-loader'
 import { languageLocales } from './constants'
 
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Wit/Oracle: The most powerful oracle, provably.',
+      meta: [
+        { charset: 'utf-8' },
+        {
+          name: 'viewport',
+          content: 'viewport-fit=cover, width=device-width, initial-scale=1',
+        },
+        {
+          hid: 'title',
+          name: 'title',
+          content: 'Wit/Oracle: The most powerful oracle, provably.',
+        },
+        {
+          hid: 'description',
+          name: 'description',
+          content:
+            'Secure your smart contracts and dApps with the Wit/Oracle: Its Proof-of-Stake (PoS) algorithm offers unmatched reliability and censorship resistance.',
+        },
+        {
+          hid: 'twitter:title',
+          name: 'twitter:title',
+          content: 'Wit/Oracle: The most powerful oracle, provably.',
+        },
+        {
+          hid: 'twitter:description',
+          name: 'twitter:description',
+          content:
+            'Secure your smart contracts and dApps with the Wit/Oracle: Its Proof-of-Stake (PoS) algorithm offers unmatched reliability and censorship resistance.',
+        },
+        {
+          hid: 'twitter:image',
+          name: 'twitter:image',
+          content: 'https://feeds.witnet.io/meta-image.png',
+        },
+        {
+          hid: 'twitter:image:alt',
+          name: 'twitter:image:alt',
+          content: 'Wit/Oracle',
+        },
+        {
+          hid: 'og:title',
+          property: 'og:title',
+          content: 'Wit/Oracle: The most powerful oracle, provably.',
+        },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content:
+            'Secure your smart contracts and dApps with the Wit/Oracle: Its Proof-of-Stake (PoS) algorithm offers unmatched reliability and censorship resistance.',
+        },
+        {
+          hid: 'og:image',
+          property: 'og:image',
+          content: 'https://witnet.io/meta-image.png',
+        },
+        {
+          hid: 'og:image:secure_url',
+          property: 'og:image:secure_url',
+          content: 'https://witnet.io/meta-image.png',
+        },
+        {
+          hid: 'og:image:alt',
+          property: 'og:image:alt',
+          content: 'Wit/Oracle',
+        },
+      ],
+      link: [
+        { rel: 'apple-touch-icon', type: 'image/x-icon', href: '/favicon.ico' },
+        {
+          rel: 'icon',
+          sizes: '180x180',
+          href: '/apple-touch-icon.png',
+          type: 'image/png',
+        },
+        {
+          rel: 'icon',
+          sizes: '32x32',
+          href: '/favicon-32x32.png',
+          type: 'image/png',
+        },
+        {
+          rel: 'icon',
+          sizes: '16x16',
+          href: '/favicon-16x16.png',
+          type: 'image/png',
+        },
+        { rel: 'icon', href: '/favicon.ico', type: 'image/x-icon' },
+        { rel: 'manifest', href: '/site.webmanifest' },
+        { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '2D2C39' },
+        { rel: 'shortcut icon', href: '/favicon.ico' },
+      ],
+    },
+  },
   devtools: { enabled: true },
   postcss: {
     plugins: {
