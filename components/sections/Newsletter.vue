58249917c94c3000cbe7a651c3d74c98fc04012a
gabaldon <maria@otherplane.com>
2024-07-19 11:27:24 +0200

    fix-newsletter

--- components/sections/Newsletter.vue
+++ components/sections/Newsletter.vue
@@ -13,24 +13,8 @@
         <DashesIcon class="w-[80px] h-auto" />
       </div>
       <div class="mt-lg">
-        <label for="subscribe_buttom" class="sr-only">Label</label>
-        <div class="flex shadow-sm">
-          <input
-            id="subscribe_buttom"
-            type="text"
-            name="subscribe_buttom"
-            placeholder="name@domain.com"
-            class="rounded-l-full min-w-[300px] sm:min-w-max py-sm px-md block w-full border-gray-200 shadow-sm text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
-          />
-          <button
-            type="button"
-            class="rounded-r-full py-sm px-md inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-e-md border border-black-950 bg-black-950 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
-          >
-            Subscribe
-          </button>
-        </div>
+        <Mailchimp />
       </div>
-      <!-- <Mailchimp /> -->
     </template>
     <template #right>
       <PalmBlackIcon class="sm:hidden lg:w-[250px] lg:h-autho rotate-180" />
