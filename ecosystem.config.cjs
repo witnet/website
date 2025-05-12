f8ddc249346b6a46b8757c1c34a5dde35bc85b82
aeweda <mraeweda@gmail.com>
2025-05-12 16:20:15 +0400

    chore: add missing ecosystem file

---
+++ ecosystem.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+    apps: [{
+      name: 'witnet-website',
+      port: '3000',
+      exec_mode: 'cluster',
+      instances: 'max',
+      script: './.output/server/index.mjs',
+      env: {
+        PORT: 3000,
+        HOST: '0.0.0.0',
+        NODE_ENV: 'production'
+      }
+    }]
+}
